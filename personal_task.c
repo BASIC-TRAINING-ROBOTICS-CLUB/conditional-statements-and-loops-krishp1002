b015f36fde56db98768468b4979b8ddb7ab60c20
Krish Parmar <151830559+krishp1002@users.noreply.github.com>
2024-09-14 06:32:34 +0530

    Create personal_task.c

---
+++ personal_task.c
@@ -0,0 +1,16 @@
+//personal task : use rational operator
+
+#include<stdio.h>
+
+int main(){
+    int kr;
+    printf("enter value : ");
+    scanf("%d",&kr);
+    if(kr>1000 && kr<2000 ){
+        printf("in range");
+    }
+    else{
+        printf("not in range");
+    }
+return 0 ;
+}
