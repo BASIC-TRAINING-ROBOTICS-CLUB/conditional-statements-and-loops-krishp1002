fbf9c5f3f692d523a5ea622d8939b099f707377a
Krish Parmar <151830559+krishp1002@users.noreply.github.com>
2024-09-14 08:35:38 +0530

    Create q9.c

---
+++ q9.c
@@ -0,0 +1,11 @@
+//print multiplication table of 3.
+
+#include<stdio.h>
+
+int main(){
+    int a=3 ;
+    for(int b=1 ; b<=10 ; b++){
+        printf("%d x %d = %d \n",a,b,a*b);
+    }
+    return 0;
+}
