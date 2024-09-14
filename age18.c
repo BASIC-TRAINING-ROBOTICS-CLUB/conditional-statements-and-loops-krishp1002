74ebc638224ea74b5dd4f5f5d9ea0e059e97eb6e
Krish Parmar <151830559+krishp1002@users.noreply.github.com>
2024-09-14 06:39:04 +0530

    Create age18.c

---
+++ age18.c
@@ -0,0 +1,17 @@
+//below 18 - not eligible //above 18 - eligible
+
+#include<stdio.h>
+
+int main(){
+    int age ;
+    printf("Enter your age : ");
+    scanf("%d",&age);
+
+    if(age>=18){
+        printf("You are eligible.");
+    }
+    else{
+        printf("You are not eligible.");
+    }
+    return 0;
+}
