3576da6f464838f57ad3e1dd00bf80a74bee4693
Krish Parmar <151830559+krishp1002@users.noreply.github.com>
2024-09-20 00:41:05 +0530

    Create freefall.h

---
+++ freefall.h
@@ -0,0 +1,9 @@
+#ifndef FREEFALL_H
+#define FREEFALL_H
+#define GRAVITY 9.81
+
+double calculate_time_of_fall(double height);
+double calculate_final_velocity(double height);
+double calculate_displacement(double time);
+
+#endif
